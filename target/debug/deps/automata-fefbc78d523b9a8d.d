dcf32e68b8ead2f69a787c504f654da3fceb0a03
León Bohn <bohn@lics.rwth-aachen.de>
2024-03-08 15:45:26 +0100

    Fix dependencies

--- target/debug/deps/automata-fefbc78d523b9a8d.d
+++
@@ -1,8 +0,0 @@
-/Users/leon/Code/automata/target/debug/deps/automata-fefbc78d523b9a8d.rmeta: src/lib.rs src/ts/mod.rs src/ts/deterministic.rs src/ts/labels.rs
-
-/Users/leon/Code/automata/target/debug/deps/automata-fefbc78d523b9a8d.d: src/lib.rs src/ts/mod.rs src/ts/deterministic.rs src/ts/labels.rs
-
-src/lib.rs:
-src/ts/mod.rs:
-src/ts/deterministic.rs:
-src/ts/labels.rs:
