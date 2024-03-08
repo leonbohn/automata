92316a4a3716c5ef1cdadc624561f13a53fea287
León Bohn <bohn@lics.rwth-aachen.de>
2024-03-08 15:31:11 +0100

    Migrate code from old repository

---
+++ target/debug/deps/automata-d5880bde37dd2c30.d
@@ -0,0 +1,8 @@
+/Users/leon/Code/automata/target/debug/deps/automata-d5880bde37dd2c30.rmeta: src/lib.rs src/ts/mod.rs src/ts/deterministic.rs src/ts/labels.rs
+
+/Users/leon/Code/automata/target/debug/deps/automata-d5880bde37dd2c30.d: src/lib.rs src/ts/mod.rs src/ts/deterministic.rs src/ts/labels.rs
+
+src/lib.rs:
+src/ts/mod.rs:
+src/ts/deterministic.rs:
+src/ts/labels.rs:
